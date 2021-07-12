5f81db38d8ea74ea7b724db7977fa1dcc3f6bd12
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-12 22:29:49 +0000

    fix init.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -92,7 +92,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN") or None
 # Custom (forked) repo URL and BRANCH for updater.
 UPSTREAM_REPO_URL = (os.environ.get("UPSTREAM_REPO_URL")
                      or "https://github.com/levina-lab/AzumiUserbot.git")
-UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "sql-extended"
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "main"
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get(
