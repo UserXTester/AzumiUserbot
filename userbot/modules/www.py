d0462b145d2b1837b8794064fdfc1c9f85d14eed
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-13 09:53:16 +0700

    w

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -18,7 +18,7 @@ from userbot.events import register
 @register(outgoing=True, pattern="^.speed$")
 async def speedtst(spd):
     """ For .speed command, use SpeedTest to check server speeds. """
-    await spd.edit("`Running speed test . . .`")
+    await spd.edit("`🚀 testing kecepatan tinggi...`")
     test = Speedtest()
 
     test.get_best_server()
@@ -71,10 +71,10 @@ async def neardc(event):
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await pong.edit("`Pong!`")
+    await pong.edit("`🏓 pong!")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit("`Pong!\n%sms`" % (duration))
+    await pong.edit("`⚡️ ping\nspeed: __%sms__`" % (duration))
 
 
 CMD_HELP.update(
