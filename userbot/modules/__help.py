6f5b245d526156f4d15cc13300f3a77229bab298
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-16 06:07:18 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -13,7 +13,7 @@ async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@azumiusersbot")
+            results = await event.client.inline_query(tgbotusername, "@gcsupportbots")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
