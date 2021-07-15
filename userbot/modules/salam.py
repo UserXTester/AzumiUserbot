a6e618754f97a1e470675f452aa3c58a502d3fec
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-15 12:41:49 +0700

    added salam

---
+++ userbot/modules/salam.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2021 By Veez Project
+# All right reserved
+
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern='^.P(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦")
+
+
+@register(outgoing=True, pattern='^.p(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦")
+
+
+@register(outgoing=True, pattern='^.L(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦")
+
+
+@register(outgoing=True, pattern='^.l(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦")
+
+
+CMD_HELP.update({
+    "salam":
+    "`𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .P`\
+\nUsage: Untuk Memberi salam.\
+\n\n`𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .L`\
+\nUsage: Untuk Menjawab Salam."
+})
