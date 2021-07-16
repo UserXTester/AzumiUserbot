be73be38db4f4fc225d9eee03e94a515e2c92011
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-16 20:15:22 +0700

    bingung error mulu dari tadi

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -1,7 +1,3 @@
-# module carbon, rewriten by levina on github
-# Copyright (C) 2021 by Veez Project
-
-
 import os
 from time import sleep
 from selenium import webdriver
@@ -18,83 +14,17 @@ TRT_LANG = "en"
 TEMP_DOWNLOAD_DIRECTORY = "/root/userbot/.bin"
 
 
-@register(outgoing=True, pattern="^.krblang (.*)")
+@register(outgoing=True, pattern="^.crblang (.*)")
 async def setlang(prog):
     global CARBONLANG
     CARBONLANG = prog.pattern_match.group(1)
     await prog.edit(f"Language for carbon.now.sh set to {CARBONLANG}")
 
 
-@register(outgoing=True, pattern="^.carbon")
+@register(outgoing=True, pattern="^.carbon1")
 async def carbon_api(e):
     """ A Wrapper for carbon.now.sh """
-    await e.edit("`memproses...`")
-    CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
-    global CARBONLANG
-    textx = await e.get_reply_message()
-    pcode = e.text
-    if pcode[8:]:
-        pcode = str(pcode[8:])
-    elif textx:
-        pcode = str(textx.message)  # Importing message to module
-    code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`memproses...\n25%`")
-    if os.path.isfile("/root/userbot/.bin/carbon.png"):
-        os.remove("/root/userbot/.bin/carbon.png")
-    url = CARBON.format(code=code, lang=CARBONLANG)
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-    prefs = {'download.default_directory': '/root/userbot/.bin'}
-    chrome_options.add_experimental_option('prefs', prefs)
-    driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
-                              options=chrome_options)
-    driver.get(url)
-    await e.edit("`memproses...\n50%`")
-    download_path = '/root/userbot/.bin'
-    driver.command_executor._commands["send_command"] = (
-        "POST", '/session/$sessionId/chromium/send_command')
-    params = {
-        'cmd': 'Page.setDownloadBehavior',
-        'params': {
-            'behavior': 'allow',
-            'downloadPath': download_path
-        }
-    }
-    driver.execute("send_command", params)
-    driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
-   # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-   # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`memproses...\n75%`")
-    # Waiting for downloading
-    while not os.path.isfile("/root/userbot/.bin/carbon.png"):
-        await sleep(0.5)
-    await e.edit("`memproses...\n100%`")
-    file = '/root/userbot/.bin/carbon.png'
-    await e.edit("`mengupload...`")
-    await e.client.send_file(
-        e.chat_id,
-        file,
-        caption="𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 [레비나](https://t.me/dlwrml),\
-        \n𝐉𝐨𝐢𝐧 𝐓𝐨 [𝐆𝐫𝐨𝐮𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/gcsupportbots)",
-        force_document=True,
-        reply_to=e.message.reply_to_msg_id,
-    )
-
-    os.remove('/root/userbot/.bin/carbon.png')
-    driver.quit()
-    # Removing carbon.png after uploading
-    await e.delete()  # Deleting msg
-
-
-@register(outgoing=True, pattern="^.c1")
-async def carbon_api(e):
-    """ A Wrapper for carbon.now.sh """
-    await e.edit("`memproses...`")
+    await e.edit("`Processing..`")
     CARBON = 'https://carbon.now.sh/?bg=rgba(249%2C237%2C212%2C0)&t=synthwave-84&wt=none&l=application%2Fjson&ds=true&dsyoff=20px&dsblur=0px&wc=true&wa=true&pv=56px&ph=0px&ln=false&fl=1&fm=IBM%20Plex%20Mono&fs=14.5px&lh=153%25&si=false&es=4x&wm=false&code={code}'
     global CARBONLANG
     textx = await e.get_reply_message()
@@ -104,7 +34,7 @@ async def carbon_api(e):
     elif textx:
         pcode = str(textx.message)  # Importing message to module
     code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`memproses...\n25%`")
+    await e.edit("`Processing..\n25%`")
     if os.path.isfile("/root/userbot/.bin/carbon.png"):
         os.remove("/root/userbot/.bin/carbon.png")
     url = CARBON.format(code=code, lang=CARBONLANG)
@@ -120,7 +50,7 @@ async def carbon_api(e):
     driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
                               options=chrome_options)
     driver.get(url)
-    await e.edit("`memproses...\n50%`")
+    await e.edit("`Processing..\n50%`")
     download_path = '/root/userbot/.bin'
     driver.command_executor._commands["send_command"] = (
         "POST", '/session/$sessionId/chromium/send_command')
@@ -135,18 +65,18 @@ async def carbon_api(e):
     driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`memproses...\n75%`")
+    await e.edit("`Processing..\n75%`")
     # Waiting for downloading
     while not os.path.isfile("/root/userbot/.bin/carbon.png"):
         await sleep(0.5)
-    await e.edit("`memproses...\n100%`")
+    await e.edit("`Processing..\n100%`")
     file = '/root/userbot/.bin/carbon.png'
-    await e.edit("`mengupload...`")
+    await e.edit("`Uploading..`")
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 [레비나](https://t.me/dlwrml),\
-        \n𝐉𝐨𝐢𝐧 𝐓𝐨 [𝐆𝐫𝐨𝐮𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/gcsupportbots)",
+        caption="Made using [Carbon](https://carbon.now.sh/about/),\
+        \na project by [Dawn Labs](https://dawnlabs.io/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -157,10 +87,10 @@ async def carbon_api(e):
     await e.delete()  # Deleting msg
 
 
-@register(outgoing=True, pattern="^.c2")
+@register(outgoing=True, pattern="^.carbon2")
 async def carbon_api(e):
     """ A Wrapper for carbon.now.sh """
-    await e.edit("`memproses...`")
+    await e.edit("`Processing..`")
     CARBON = 'https://carbon.now.sh/?bg=rgba(239%2C40%2C44%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}'
     global CARBONLANG
     textx = await e.get_reply_message()
@@ -170,7 +100,7 @@ async def carbon_api(e):
     elif textx:
         pcode = str(textx.message)  # Importing message to module
     code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`memproses...\n25%`")
+    await e.edit("`Processing..\n25%`")
     if os.path.isfile("/root/userbot/.bin/carbon.png"):
         os.remove("/root/userbot/.bin/carbon.png")
     url = CARBON.format(code=code, lang=CARBONLANG)
@@ -186,7 +116,7 @@ async def carbon_api(e):
     driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
                               options=chrome_options)
     driver.get(url)
-    await e.edit("`memproses...\n50%`")
+    await e.edit("`Processing..\n50%`")
     download_path = '/root/userbot/.bin'
     driver.command_executor._commands["send_command"] = (
         "POST", '/session/$sessionId/chromium/send_command')
@@ -201,18 +131,18 @@ async def carbon_api(e):
     driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`memproses...\n75%`")
+    await e.edit("`Processing..\n75%`")
     # Waiting for downloading
     while not os.path.isfile("/root/userbot/.bin/carbon.png"):
         await sleep(0.5)
-    await e.edit("`memproses...\n100%`")
+    await e.edit("`Processing..\n100%`")
     file = '/root/userbot/.bin/carbon.png'
-    await e.edit("`mengupload...`")
+    await e.edit("`Uploading..`")
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 [레비나](https://t.me/dlwrml),\
-        \n𝐉𝐨𝐢𝐧 𝐓𝐨 [𝐆𝐫𝐨𝐮𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/gcsupportbots)",
+        caption="Made using [Carbon](https://carbon.now.sh/about/),\
+        \na project by [Dawn Labs](https://dawnlabs.io/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -223,10 +153,10 @@ async def carbon_api(e):
     await e.delete()  # Deleting msg
 
 
-@register(outgoing=True, pattern="^.c3")
+@register(outgoing=True, pattern="^.carbon3")
 async def carbon_api(e):
     """ A Wrapper for carbon.now.sh """
-    await e.edit("`memproses...`")
+    await e.edit("`Processing..`")
     CARBON = 'https://carbon.now.sh/?bg=rgba(74%2C144%2C226%2C1)&t=material&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Fira%20Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code={code}'
     global CARBONLANG
     textx = await e.get_reply_message()
@@ -236,7 +166,7 @@ async def carbon_api(e):
     elif textx:
         pcode = str(textx.message)  # Importing message to module
     code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`memproses...\n25%`")
+    await e.edit("`Processing..\n25%`")
     if os.path.isfile("/root/userbot/.bin/carbon.png"):
         os.remove("/root/userbot/.bin/carbon.png")
     url = CARBON.format(code=code, lang=CARBONLANG)
@@ -252,7 +182,7 @@ async def carbon_api(e):
     driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
                               options=chrome_options)
     driver.get(url)
-    await e.edit("`memproses...\n50%`")
+    await e.edit("`Processing..\n50%`")
     download_path = '/root/userbot/.bin'
     driver.command_executor._commands["send_command"] = (
         "POST", '/session/$sessionId/chromium/send_command')
@@ -267,18 +197,18 @@ async def carbon_api(e):
     driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`memproses...\n75%`")
+    await e.edit("`Processing..\n75%`")
     # Waiting for downloading
     while not os.path.isfile("/root/userbot/.bin/carbon.png"):
         await sleep(0.5)
-    await e.edit("`memproses...\n100%`")
+    await e.edit("`Processing..\n100%`")
     file = '/root/userbot/.bin/carbon.png'
-    await e.edit("`mengupload...`")
+    await e.edit("`Uploading..`")
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 [레비나](https://t.me/dlwrml),\
-        \n𝐉𝐨𝐢𝐧 𝐓𝐨 [𝐆𝐫𝐨𝐮𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/gcsupportbots)",
+        caption="Made using [Carbon](https://carbon.now.sh/about/),\
+        \na project by [Dawn Labs](https://dawnlabs.io/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -289,10 +219,10 @@ async def carbon_api(e):
     await e.delete()  # Deleting msg
 
 
-@register(outgoing=True, pattern="^.c4")
+@register(outgoing=True, pattern="^.carbon4")
 async def carbon_api(e):
     """ A Wrapper for carbon.now.sh """
-    await e.edit("`memproses...`")
+    await e.edit("`Processing..`")
     CARBON = 'https://carbon.now.sh/?bg=rgba(29%2C40%2C104%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}'
     global CARBONLANG
     textx = await e.get_reply_message()
@@ -302,7 +232,7 @@ async def carbon_api(e):
     elif textx:
         pcode = str(textx.message)  # Importing message to module
     code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`memproses...\n25%`")
+    await e.edit("`Processing..\n25%`")
     if os.path.isfile("/root/userbot/.bin/carbon.png"):
         os.remove("/root/userbot/.bin/carbon.png")
     url = CARBON.format(code=code, lang=CARBONLANG)
@@ -318,7 +248,7 @@ async def carbon_api(e):
     driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
                               options=chrome_options)
     driver.get(url)
-    await e.edit("`memproses...\n50%`")
+    await e.edit("`Processing..\n50%`")
     download_path = '/root/userbot/.bin'
     driver.command_executor._commands["send_command"] = (
         "POST", '/session/$sessionId/chromium/send_command')
@@ -333,18 +263,18 @@ async def carbon_api(e):
     driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`memproses...\n75%`")
+    await e.edit("`Processing..\n75%`")
     # Waiting for downloading
     while not os.path.isfile("/root/userbot/.bin/carbon.png"):
         await sleep(0.5)
-    await e.edit("`memproses...\n100%`")
+    await e.edit("`Processing..\n100%`")
     file = '/root/userbot/.bin/carbon.png'
-    await e.edit("`mengupload...`")
+    await e.edit("`Uploading..`")
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 [레비나](https://t.me/dlwrml),\
-        \n𝐉𝐨𝐢𝐧 𝐓𝐨 [𝐆𝐫𝐨𝐮𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/gcsupportbots)",
+        caption="Made using [Carbon](https://carbon.now.sh/about/),\
+        \na project by [Dawn Labs](https://dawnlabs.io/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -355,10 +285,10 @@ async def carbon_api(e):
     await e.delete()  # Deleting msg
 
 
-@register(outgoing=True, pattern="^.c5")
+@register(outgoing=True, pattern="^.carbon5")
 async def carbon_api(e):
     """ A Wrapper for carbon.now.sh """
-    await e.edit("`memproses...`")
+    await e.edit("`Processing..`")
     CARBON = 'https://carbon.now.sh/?bg=rgba(76%2C144%2C140%2C1)&t=night-owl&wt=none&l=coffeescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Fira%20Code&fs=14px&lh=133%25&si=false&es=2x&wm=false&code={code}'
     global CARBONLANG
     textx = await e.get_reply_message()
@@ -368,7 +298,7 @@ async def carbon_api(e):
     elif textx:
         pcode = str(textx.message)  # Importing message to module
     code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`memproses...\n25%`")
+    await e.edit("`Processing..\n25%`")
     if os.path.isfile("/root/userbot/.bin/carbon.png"):
         os.remove("/root/userbot/.bin/carbon.png")
     url = CARBON.format(code=code, lang=CARBONLANG)
@@ -384,7 +314,7 @@ async def carbon_api(e):
     driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
                               options=chrome_options)
     driver.get(url)
-    await e.edit("`memproses...\n50%`")
+    await e.edit("`Processing..\n50%`")
     download_path = '/root/userbot/.bin'
     driver.command_executor._commands["send_command"] = (
         "POST", '/session/$sessionId/chromium/send_command')
@@ -399,18 +329,18 @@ async def carbon_api(e):
     driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`memproses...\n75%`")
+    await e.edit("`Processing..\n75%`")
     # Waiting for downloading
     while not os.path.isfile("/root/userbot/.bin/carbon.png"):
         await sleep(0.5)
-    await e.edit("`memproses...\n100%`")
+    await e.edit("`Processing..\n100%`")
     file = '/root/userbot/.bin/carbon.png'
-    await e.edit("`mengupload...`")
+    await e.edit("`Uploading..`")
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 [레비나](https://t.me/dlwrml),\
-        \n𝐉𝐨𝐢𝐧 𝐓𝐨 [𝐆𝐫𝐨𝐮𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/gcsupportbots)",
+        caption="Made using [Carbon](https://carbon.now.sh/about/),\
+        \na project by [Dawn Labs](https://dawnlabs.io/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -423,8 +353,6 @@ async def carbon_api(e):
 
 CMD_HELP.update({
     "carbon":
-    "Command : `.carbon` <Text/Reply>\
-    \nUsage : atau anda juga bisa ketik .c1 atau 2,3,4,5 untuk mengganti warna background anda.\
-    \n\nCommand : .krblang <text>\
-    \nUsage : Modifikasi teks anda menggunakan carbon.\nUse .krblang <text> untuk mengatur bahasa untuk kode Anda."
+    "`.carbon`value <values=1,2,3,4,5>\
+        \nUsage:reply or type .carbon1 or 2,3,4,5 value and beautify your text."
 })
