7eb732c653cc4425c5a229cef5a3377cb3469c53
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-17 14:18:01 +0700

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 """
-Userbot module to help you manage a group
+Userbot module to help you manage a group as admin
 """
 
 from asyncio import sleep
